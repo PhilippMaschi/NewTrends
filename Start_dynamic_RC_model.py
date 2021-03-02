ca398890dfa2ed9e663764d72c780a0a7ec0fb23
mascherbauer <phil.masch@gmx.at>
2021-03-02 14:09:54 +0100

    Start_dynamic_RC_model.py: paths changed

--- Start_dynamic_RC_model.py
+++ Start_dynamic_RC_model.py
@@ -2,13 +2,12 @@ from pathlib import Path
 from Heatdemand_rc_model import Heatdemand_rc_model
 
 # define if new data is calculated or just loaded:
-load_data = True
+load_data = False
 print("load data is set to " + str(load_data) + '\n')
 
 # Read data:
 # base_results_path = Path('V:/projects/2020_RES_H/invert/output_20201111/AUT')
-base_results_path = Path('L:/projekte/_abgeschlossen/2015_P2H-Pot/Invert_Szenarien/Invert_output_new')
-input_data_path = Path('C:/Users/mascherbauer/PycharmProjects/NewTrends/inputdata')
+base_results_path = Path('C:/Users/mascherbauer/PycharmProjects/NewTrends/inputdata')
 
 # define scenarios:
 # scenariolist = [r"_scen_aut_cheetah_ref_install_iopt_dh_95"]
@@ -24,9 +23,9 @@ grid_reference_year = 2010
 for scn in scenariolist:
     results_path = base_results_path / scn
     run_number_str = "001"
-    results_path_rcm = results_path / "ADD_RESULTS" / "Dynamic_Calc_Input_Data"
-    results_path_temperatures = results_path / "ADD_RESULTS" / "Annual_Climate_Data"
-    results_path_FWKWK_data = results_path / "ADD_RESULTS" / "FWKWK_REGIONAL_DATA"
+    results_path_rcm = results_path / "Dynamic_Calc_Input_Data"
+    results_path_temperatures = results_path / "Annual_Climate_Data"
+    results_path_FWKWK_data = results_path / "FWKWK_REGIONAL_DATA"
 
     # check if paths exist:
     print("rcm path exists: " + str(results_path_rcm.exists()))
